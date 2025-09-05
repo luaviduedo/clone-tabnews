f87c079ac59fa60e50a5677ae45df9a893997504
Luã Viduedo <99000420+luaviduedo@users.noreply.github.com>
2025-09-05 20:19:13 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a você, mas como isso não é possível, dedico este cantinho especialmente para você. Te amo! 💚</h1>
+    return <h1>💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a você, mas como isso não é possível, dedico este cantinho especialmente para você. Te amo! 💚</h1>
 }
 
 export default Home 
