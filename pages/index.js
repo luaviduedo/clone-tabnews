88eeebdf04caad4fd240e4e8ec90ebbaf002c7ce
Luã Viduedo <99000420+luaviduedo@users.noreply.github.com>
2025-09-09 21:18:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a você, mas como isso não é possível, dedico este cantinho especialmente para você. Te amo! 💚</h1>
+  return (
+    <h1>
+      💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o
+      quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a
+      você, mas como isso não é possível, dedico este cantinho especialmente
+      para você. Te amo! 💚
+    </h1>
+  );
 }
 
-export default Home 
+export default Home;
