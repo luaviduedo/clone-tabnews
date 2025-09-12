01f27c048d043453170aa7f5c53e7841d8d21f3c
Luã Viduedo <99000420+luaviduedo@users.noreply.github.com>
2025-09-12 23:49:54 +0000

    arrumando error na mensagem de main page

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,14 @@ function Home() {
     💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o
     quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a
     você, mas como isso não é possível, dedico este cantinho especialmente
-    para você. Te amo! 💚
-  </h1>
-  <p>
+    para você. Te amo! 
     Um dia sem você é um dia sem graça. É como acordar e perceber que o sol,
     que ilumina tudo e traz vida ao mundo, simplesmente deixou de existir.
     Eu não entendo por que isso aconteceu, mas, se de alguma forma fui o
     motivo, quero pedir sinceras desculpas. Eu nunca quis que as coisas
-    chegassem a esse ponto… Só quero minha melhor amiga de volta.
-  </p>
+    chegassem a esse ponto… Só quero minha melhor amiga de volta. 
+    💚
+  </h1>
   );
 }
 
