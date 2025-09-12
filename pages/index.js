ee916aa4a54f221b67299d5eae9ad914827e6a85
Luã Viduedo <99000420+luaviduedo@users.noreply.github.com>
2025-09-12 23:46:44 +0000

    mudando mensagem de main page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
 function Home() {
   return (
-    (
-      <h1>
-        💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o
-        quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a
-        você, mas como isso não é possível, dedico este cantinho especialmente
-        para você. Te amo! 💚
-      </h1>
-    ),
-    (
-      <p>
-        Um dia sem você é um dia sem graça. É como acordar e perceber que o sol,
-        que ilumina tudo e traz vida ao mundo, simplesmente deixou de existir.
-        Eu não entendo por que isso aconteceu, mas, se de alguma forma fui o
-        motivo, quero pedir sinceras desculpas. Eu nunca quis que as coisas
-        chegassem a esse ponto… Só quero minha melhor amiga de volta.
-      </p>
-    )
+  <h1>
+    💚 Oi, Kaka! Você é muito especial para mim. Talvez você nem imagine o
+    quão incrível é. Se eu pudesse, dedicaria todas as páginas da internet a
+    você, mas como isso não é possível, dedico este cantinho especialmente
+    para você. Te amo! 💚
+  </h1>,
+  <p>
+    Um dia sem você é um dia sem graça. É como acordar e perceber que o sol,
+    que ilumina tudo e traz vida ao mundo, simplesmente deixou de existir.
+    Eu não entendo por que isso aconteceu, mas, se de alguma forma fui o
+    motivo, quero pedir sinceras desculpas. Eu nunca quis que as coisas
+    chegassem a esse ponto… Só quero minha melhor amiga de volta.
+  </p>
   );
 }
 
