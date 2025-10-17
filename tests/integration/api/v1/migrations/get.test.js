af6df6a128279d403fca16359f059c16abbdcd33
Luã Viduedo <99000420+luaviduedo@users.noreply.github.com>
2025-10-17 16:47:10 +0000

    create `jest.config.js` make tests more robust and flexible

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from "infra/database.js";
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -6,4 +8,5 @@ test("GET to /api/v1/migrations should return 200", async () => {
   console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
