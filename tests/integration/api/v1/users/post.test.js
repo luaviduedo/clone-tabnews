f33c52a63fa96e826beb762ce67641025a2a56e1
Lua Viduedo <luaviduedo@gmail.com>
2025-12-19 17:00:49 -0300

    feat: hash `password` in `user.create`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -30,7 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "lua.vdd",
         email: "Contatolua@gmail.com",
-        password: "senha123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -38,6 +40,14 @@ describe("POST /api/v1/users", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+
+      const userInDataBase = await user.findOneByUsername("lua.vdd");
+      const correctPasswordMatch = await password.compare(
+        "senha123",
+        userInDataBase.password,
+      );
+
+      expect(correctPasswordMatch).toBe(true);
     });
 
     test("With duplicated 'email'", async () => {
